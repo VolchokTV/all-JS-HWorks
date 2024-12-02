fde82b4c04544897bed97ece04b8be82f0be64a0
Alex Vorobev <avbavnn@gmail.com>
2024-12-02 22:55:53 +0300

    css add

--- hw9_dom/script.js
+++ hw9_dom/script.js
@@ -6,16 +6,17 @@ const input = document.createElement("input");
 
 const list = document.createElement("ul");
 
-const listItem = document.createElement("li");
+const listItem1 = document.createElement("li");
+listItem1.innerText = 'To Do task1';
 const listItem2 = document.createElement("li");
-
+listItem2.innerText = 'To Do task2';
 const btn = document.createElement("button");
 btn.innerText = "To Do";
 
 document.body.prepend(header);
 header.after(container);
 container.append(list);
-list.append(listItem);
+list.append(listItem1);
 list.append(listItem2);
 container.append(input);
 container.append(btn);
